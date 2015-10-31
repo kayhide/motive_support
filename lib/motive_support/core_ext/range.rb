13bdb4b03b84b8471ce38e45c57dcfde3b505adf
kayhide <kayhide@gmail.com>
2015-10-31 11:05:51 +0100

    remove compatible files from core_ext/{range,regexp}.

--- lib/motive_support/core_ext/range.rb
+++ lib/motive_support/core_ext/range.rb
@@ -7,5 +7,4 @@ require 'motive_support/rake_tasks'
 require 'motive_support/hooks'
 
 require 'motive_support/ext'
-require 'active_support/core_ext/range/include_range'
-require 'active_support/core_ext/range/overlaps'
+require 'active_support/core_ext/range'
