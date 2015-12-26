b7a918f1c2014fc6606b78f426f34268dab6fd72
kayhide <kayhide@gmail.com>
2015-12-27 00:58:12 +0700

    update i18n hook.

--- lib/motive_support/hooks/i18n.rb
+++ lib/motive_support/hooks/i18n.rb
@@ -2,5 +2,8 @@ MotionBlender.on_require 'active_support/i18n' do
   require 'i18n'
 
   src = File.expand_path('../../locale/en.yml', __FILE__)
-  I18n.load_path << [src, 'locale/active_support/en.yml']
+  item = [src, 'locale/active_support/en.yml']
+  unless I18n.load_path.include? item
+    I18n.load_path << item
+  end
 end
